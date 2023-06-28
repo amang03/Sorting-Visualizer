742b4de357f528d54af845f9ef612c1ad37f4591
@amang02 <127767031+amang03@users.noreply.github.com>
2023-06-28 16:39:44 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -79,24 +79,3 @@ function runalgo()
                         break;
     }
 }
-let audioCtx=null;
-function playNote(freq){
-    if(audioCtx==null){
-        audioCtx=new(
-            AudioContext ||
-            webkitAudioContext ||
-            window.webkitAudioContext)();
-    }
-    const dur=0.1;
-    const osc=audioCtx.createOscillator();
-    osc.frequency.value=freq;
-    osc.start();
-    osc.stop(audio.Ctx.currentTime+dur);
-    const node=audioCtx.createGain();
-    node.gain.value=0.1;
-    node.gain.linearRampToValueAtTime(
-        0,audioCtx.currentTime+dur
-    );
-osc.connect(node);
-node.connect(audioCtx.destination);    
-}
